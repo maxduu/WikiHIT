01c27d14e5e032a9e4d1f6f7d46f5d89aa620af0
noxydude <60411452+noxydude@users.noreply.github.com>
2021-04-20 15:41:05 -0400

    updated with urls

--- data/json_to_csv.py
+++ data/json_to_csv.py
@@ -1,21 +1,25 @@
 
 import json
 import csv
+import pickle
 import random
 
-randomize_goals = True
+randomize_goals = False
 
 json_file = 'para_step_goal_links_gold'
 filename = json_file + '.json'
 
 with open(filename) as f:
-  data = json.load(f)
+    data = json.load(f)
+  
+with open("en_title_url.pkl", 'rb') as f:
+    url_mapping = pickle.load(f)
 
 rows = []
 
-for goal, entry in data.items():
+for step, entry in data.items():
     l = []
-    l.append(goal)
+    l.append(step)
     l.append(entry['corresponding_goal'])
     l.append(entry['gold_goal'])
     retrieved_goals_list = entry['retrieved_goals']
@@ -32,6 +36,22 @@ for goal, entry in data.items():
         l.append(retrieved_goals_similarity_list[i])
     
     l.append(entry['retrieved_goal_rank'])
+        
+    if entry['corresponding_goal'] in url_mapping:
+        l.append(url_mapping[entry['corresponding_goal']])
+    else:
+        l.append('')
+    
+    if entry['gold_goal'] in url_mapping:
+        l.append(url_mapping[entry['gold_goal']])
+    else:
+        l.append('')
+    
+    for i in range(len(retrieved_goals_list)):
+        if retrieved_goals_list[i] in url_mapping:
+            l.append(url_mapping[retrieved_goals_list[i]])
+        else:
+            l.append('')
     
     rows.append(l)
     
@@ -45,7 +65,7 @@ else:
 
 with open(filename_out, 'w', newline='', encoding='utf-8') as out:
     csv_out=csv.writer(out)
-    csv_out.writerow(['goal',
+    csv_out.writerow(['step',
                       'corresponding_goal',
                       'gold_goal',
                       'retrieved_goal_1', 'retrieved_goal_2',
@@ -58,7 +78,14 @@ with open(filename_out, 'w', newline='', encoding='utf-8') as out:
                       'retrieved_goal_similarity_5', 'retrieved_goal_similarity_6',
                       'retrieved_goal_similarity_7', 'retrieved_goal_similarity_8',
                       'retrieved_goal_similarity_9', 'retrieved_goal_similarity_10',
-                      'retrieved_goal_rank'
+                      'retrieved_goal_rank',
+                      'corresponding_goal_url',
+                      'gold_goal_url',
+                      'retrieved_goal_url_1', 'retrieved_goal_url_2',
+                      'retrieved_goal_url_3', 'retrieved_goal_url_4',
+                      'retrieved_goal_url_5', 'retrieved_goal_url_6',
+                      'retrieved_goal_url_7', 'retrieved_goal_url_8',
+                      'retrieved_goal_url_9', 'retrieved_goal_url_10',
                       ])
     csv_out.writerows(rows)
         
